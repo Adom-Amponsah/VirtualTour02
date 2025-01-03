ef98447359afa4ae81c7845ffd89d1c054357ea6
Adom Isaac <adomisaac50@gmail.com>
2025-01-03 14:50:50 +0000

    final implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ApartmentList from './components/ApartmentList';
 import ApartmentDetail from './components/ApartmentDetail';
 import ReservationCheckout from './components/ReservationCheckout';
@@ -8,13 +8,13 @@ import { BookingProvider } from './context/BookingContext';
 const App = () => {
   return (
     <BookingProvider>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<ApartmentList />} />
           <Route path="/apartment/:id" element={<ApartmentDetail />} />
           <Route path="/checkout" element={<ReservationCheckout />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </BookingProvider>
   );
 };
